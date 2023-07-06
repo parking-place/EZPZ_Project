92830abf667a67a43b2a7c31ad3e27701d0cfdeb
parking <hyenzzang94@gmail.com>
2023-07-06 07:39:37 +0000

    23.07.06

--- modules/torchmodules/FinBertKR.py
+++ modules/torchmodules/FinBertKR.py
@@ -2,8 +2,6 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch.nn.functional as F
 import torch
-import re
-
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -14,9 +12,15 @@ class FinBertKR:
         model_name = "snunlp/KR-FinBert-SC"
         self.__tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.__model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.__model.to(DEVICE)
         self.__softmax = F.softmax
         
     def load_dict(self, model_name):
         self.__model.load_state_dict(torch.load(f'{DICT_PATH}/{model_name}'))
-        
-    
+
+    def predict(self, text):
+        inputs = self.__tokenizer(text, return_tensors='pt').to(DEVICE)
+        outputs = self.__model(**inputs)
+        probs = self.__softmax(outputs.logits, dim=1)
+        pred = self.__model.config.id2label[probs.argmax().item()]
+        return pred
