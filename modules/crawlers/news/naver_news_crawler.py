fb496a258f401c2cfb78108e48d4c56a7057b7c7
Parking_place <hyenzzang94@gmail.com>
2023-08-03 12:37:34 +0900

    23.08.03

--- modules/crawlers/news/naver_news_crawler.py
+++ modules/crawlers/news/naver_news_crawler.py
@@ -19,50 +19,55 @@ def get_url(comp_name, page=1):
     url = BASE_URL.format(comp_name=comp_name, page=page)
     return url
 
-def get_news_in_page(comp_name, page=1):
+async def get_news_in_page(comp_name, session, page=1):
     url = get_url(comp_name, page)
-    r = requests.get(url)
-    soup = bs(r.text, 'lxml')
-    # #main_pack > section > div > div.group_news > ul
-    mainbox_list = soup.select('#main_pack > section > div > div.group_news > ul > li')
-    # li class : sub_bx
-    subbox_list = soup.find_all('li', {'class':'sub_bx'})
-    
-    link_list = []
-    
-    for box in mainbox_list:
-        a_tag = box.select_one('div.news_wrap.api_ani_send > div > a')
-        link = a_tag['href']
-        link_list.append(link)
-    
-    for box in subbox_list:
-        a_tag = box.select_one('span > a')
-        link = a_tag['href']
-        link_list.append(link)
+    async with session.get(url, headers=HEADERS) as r:
+        html = await r.text()
+        soup = bs(html, 'lxml')
+        # #main_pack > section > div > div.group_news > ul
+        mainbox_list = soup.select('#main_pack > section > div > div.group_news > ul > li')
+        # li class : sub_bx
+        subbox_list = soup.find_all('li', {'class':'sub_bx'})
+        
+        link_list = []
+        
+        for box in mainbox_list:
+            a_tag = box.select_one('div.news_wrap.api_ani_send > div > a')
+            link = a_tag['href']
+            link_list.append(link)
+        
+        for box in subbox_list:
+            a_tag = box.select_one('span > a')
+            link = a_tag['href']
+            link_list.append(link)
 
-    news_dict = {
-        'link': [],
-        'content': [],
-        'date': [],
-    }
-    
-    results = asyncio.run(nc.get_content_async(link_list))
-    
-    for result in results:
-        link, content, date = result
-        news_dict['link'].append(link)
-        news_dict['content'].append(content)
-        news_dict['date'].append(date)
-    
-    news_df = pd.DataFrame(news_dict)
+        news_dict = {
+            'link': [],
+            'content': [],
+            'date': [],
+        }
+        
+        results = await nc.get_content_async(link_list)
+        
+        for result in results:
+            link, content, date = result
+            news_dict['link'].append(link)
+            news_dict['content'].append(content)
+            news_dict['date'].append(date)
+        
+        news_df = pd.DataFrame(news_dict)
     
     return news_df
 
+async def async_loop_outer(comp_name, page=5):
+    async with aiohttp.ClientSession(headers=HEADERS) as session:
+        result_list = await asyncio.gather(*[get_news_in_page(comp_name, session, i) for i in range(1, page+1)])
+    return result_list
+
 def get_news(comp_name, page=5):
-    news_dfs = []
-    for i in range(1, page+1):
-        news_dfs.append(get_news_in_page(comp_name, i))
-    news_df = pd.concat(news_dfs)
+    result_list = asyncio.run(async_loop_outer(comp_name, page))
+    result_list = [result for result in result_list if result is not None]
+    news_df = pd.concat(result_list)
     news_df = pretreatment_data(news_df)
     # save_news(news_df, comp_name)
     return news_df
@@ -80,3 +85,4 @@ if __name__ == '__main__':
     df = get_news('SK 하이닉스', 5)
     
     print(df.head())
+    print(df.shape)
