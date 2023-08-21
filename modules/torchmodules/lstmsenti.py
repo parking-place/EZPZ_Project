6606bba3ae9dc0267aea808a1ddf100116d80187
parking-place <hyenzzang94@gmail.com>
2023-08-21 20:44:33 +0900

    23.08.21

--- modules/torchmodules/lstmsenti.py
+++ modules/torchmodules/lstmsenti.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import torch
-import lstmmodel
+from lstmmodel import LSTMModel
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -8,7 +8,7 @@ DICT_PATH = '/home/parking/ml/data/MiniProj/models'
 
 class LSTMSenti:
     def __init__(self):
-        self.__model = lstmmodel.LSTMModel()
+        self.__model = LSTMModel()
         pass
     
     def get_sentiment(self, text):
