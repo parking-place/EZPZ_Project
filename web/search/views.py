ca4d650678eedcf926400acca7441fed0036b4d5
prussian-1to9 <prussian.3to3@gmail.com>
2023-08-21 15:25:53 +0900

    23.08.21 modified : search query

--- web/search/views.py
+++ web/search/views.py
@@ -10,17 +10,13 @@ def main_page(request):
         }
         
         # form에서 넘어온건지 체크
-        comp_name_list = [el.comp_name for el in CompInfo.objects.all()] # 전체 회사 이름 리스트
         comp_name = request.GET.get('q', None)
         
         # 파라미터가 있는 경우
         if comp_name:
             try:
-                # 검색어 보정
-                comp_name = [el for el in comp_name_list if el.find(comp_name) > -1][0]
-                
                 # comp_uid로 변환시키기
-                comp_uid = CompInfo.objects.filter(comp_name=comp_name)[0].comp_uid
+                comp_uid = CompInfo.objects.get(comp_name__contains=comp_name).comp_uid
                 
                 # 기업정보 페이지로 이동
                 return redirect(f'comp/?comp_uid={comp_uid}')
