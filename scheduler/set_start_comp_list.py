ea96568c38bd6642935aae7db525a52081c3105e
Parking-place <hyenzzamg94@gmail.com>
2023-08-25 12:05:10 +0000

    23.08.25

--- scheduler/set_start_comp_list.py
+++ scheduler/set_start_comp_list.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     
     for comp_name, jp_uid in zip(comp_list, jp_uid_list):
         sql = 'INSERT INTO comp_info '
-        sql += '(comp_name, comp_loc, comp_thumb, comp_cont, comp_founded, comp_size, comp_url, is_reged, comp_jpuid, comp_ct_uid, create_date, modify_date) '
+        sql += '(comp_name, comp_loc, comp_thumb, comp_cont, comp_founded, comp_size, comp_url, is_reged, comp_jpuid, comp_ctuid, create_date, modify_date) '
         sql += f'VALUES ("{comp_name}", "null", "null", "null", "null", "null", "null", "N", "{jp_uid}", "null", "{create_date}", "{modify_date}")'
         
         sc.conn_and_exec(sql)
