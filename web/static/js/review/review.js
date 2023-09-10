a9e1c9f7017ecb523044120a6715d12fd0d963bc
prussian-1to9 <prussian.3to3@gmail.com>
2023-09-10 20:24:58 +0900

    23.09.10 added : detail box, graph animation

--- web/static/js/review/review.js
+++ web/static/js/review/review.js
@@ -1,35 +1,3 @@
-/*  ========================================
-    Sub_detail_menu 관련
-    ======================================== */
-/*
-function openMenu(evt, menuName) {
-    var i, tabcontent, tablinks;
-
-    // 1. 모든 탭 컨텐츠 숨기기.
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    
-    // 2. 모든 버튼에서 active 클래스 제거.
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
-
-    // 3. 선택된 메뉴와 관련된 탭 컨텐츠를 보여주고 버튼에 active 클래스 추가.
-    // 3-1. 현재 클릭한 클래스 이름에 active 클래스 없으면 참이라 {...}안에있는 조건문 실행. " "공백을 넣어야지 내가 원하는대로 나옴. 
-    document.getElementById(menuName).style.display = "block";
-    if (!evt.currentTarget.classList.contains('active')) {
-        evt.currentTarget.className += " active";
-    }
-}
-
-// 4. 첫 번째 탭을 기본적으로 열어줍니다.
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementsByClassName('tablinks')[0].click();
-});
-*/
 // 리뷰 페이지 내 세부 탭 이동
 function go_page(type, comp_uid){
     if(type && type=='half'){
@@ -38,11 +6,74 @@ function go_page(type, comp_uid){
         document.location = '/review/quarter?comp_uid=' + comp_uid;
     }
 }
+
+// 더보기 버튼 클릭 시
+function show_details(){
+    $('.review_cont').css('height', 'auto');
+    $('#details_box').css('display', 'none');
+}
+
 /*  ========================================
     Graph 관련
     ======================================== */
+// hoverValue : show keyword animation
+const hoverValue = {
+    id: 'hoverValue',
+    afterDatasetsDraw(chart, args, pluginOptions) {
+        const { ctx, data, options } = chart;
+        chart.getActiveElements().forEach((active) => {
+            value = '키워드 : ' + time_keywords[active.index];
+            ctx.fillText(value, window.innerWidth * 0.02, 280);
+        })
+    }
+}
 
+// chart drawing function
+function draw_chart(cols, rate){
+    const data = {
+        labels: cols,
+        datasets: [
+            {
+                label: '평균 별점'
+                , data: rate
+                , borderColor : 'skyblue'
+            }
+        ]
+    };
+
+    const config = {
+        type: 'line'
+        , data: data
+        , options: {
+            responsive: false
+            , plugins: {
+                legend: { display: false }
+                , title: {
+                    display: true,
+                    text: '리뷰 통계'
+                }
+            }
+            , scales: {
+                x: { display: false }
+                , y: {
+                    suggestedMin: 0
+                    , suggestedMax: 5
+                    , ticks: { stepSize: 1 }
+                }
+            }
+        }
+        , plugins: [hoverValue]
+    };
+    const el = document.getElementById('review_chart'); // 차트 들어갈 element
+    const pieChart = new Chart(el, config);
+}
 
 /*  ========================================
     Wordcloud 관련
     ======================================== */
+function draw_word_cloud(data){
+    var chart = anychart.tagCloud(data);
+    chart.angles([0]);
+    chart.container("review_word_cloud");
+    chart.draw();
+}
